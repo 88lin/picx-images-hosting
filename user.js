0266949b2d704c4501bc58fba04bd9dfb725e9ba
茉灵智库 <111191349+88lin@users.noreply.github.com>
2025-11-05 20:57:50 +0800

    Create user.js

---
+++ user.js
@@ -0,0 +1,15 @@
+/ ==UserScript==
+// @name         6666666666666
+// @namespace    http://tampermonkey.net/
+// @version      1.6
+// @author       https://nuaa.tech/
+// @icon         https://nuaa.tech/zz.svg
+// @grant             unsafeWindow
+// @grant             GM_addStyle
+// @grant             GM_openInTab
+// @grant             GM_getValue
+// @grant             GM_setValue
+// @grant             GM_xmlhttpRequest
+// @charset		      UTF-8
+// @license           GPL License
+// ==/UserScript==
