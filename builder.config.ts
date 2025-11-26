0594839180d85215ce39c9b2731b1b7031f0baa0
茉灵智库 <111191349+88lin@users.noreply.github.com>
2025-11-27 07:14:48 +0800

    Create builder.config.ts

---
+++ builder.config.ts
@@ -0,0 +1,13 @@
+import { defineBuilderConfig } from '@afilmory/builder'
+
+export default defineBuilderConfig(() => ({
+  storage: {
+    provider: 'github',
+    owner: '88lin',
+    repo: 'picx-images-hosting',
+    branch: 'master',
+    path: '',
+    useRawUrl: false,
+    cdnUrl: 'https://jsd.akass.cn/gh/88lin/picx-images-hosting@master',
+  },
+}))
